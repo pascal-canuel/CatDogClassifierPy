d5e688cfda9fb9f3454098ee9ca9f3c2f5ec5e04
Pascal Canuel <pascalcanuel@hotmail.com>
2018-11-15 18:01:22 -0500

    Add files via upload

---
+++ CatDogClassifier/CatDogClassifier.py
@@ -0,0 +1,15 @@
+'''
+Cat & Dog classifier
+By: Pascal Canuel, William Garneau & Isaac Fiset
+
+Dataset:
+https://www.kaggle.com/tongpython/cat-and-dog
+https://www.kaggle.com/c/dogs-vs-cats/data
+
+Links:
+https://github.com/ardamavi/Dog-Cat-Classifier
+
+https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+https://www.kaggle.com/gurjarboy/cnn-basic-cat-vs-dog
+https://becominghuman.ai/building-an-image-classifier-using-deep-learning-in-python-totally-from-a-beginners-perspective-be8dbaf22dd8
+'''
