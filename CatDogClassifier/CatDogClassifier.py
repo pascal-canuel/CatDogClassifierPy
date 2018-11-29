159e48f9c383ca9949b74765f898ec64f682c9d8
Pascal Canuel <pascalcanuel@hotmail.com>
2018-11-29 17:03:29 -0500

    Update CatDogClassifier.py

--- CatDogClassifier/CatDogClassifier.py
+++ CatDogClassifier/CatDogClassifier.py
@@ -10,8 +10,6 @@ Links:
 https://github.com/ardamavi/Dog-Cat-Classifier
 
 https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
-https://www.kaggle.com/gurjarboy/cnn-basic-cat-vs-dog
-https://becominghuman.ai/building-an-image-classifier-using-deep-learning-in-python-totally-from-a-beginners-perspective-be8dbaf22dd8
 '''
 
 import tensorflow as tf
